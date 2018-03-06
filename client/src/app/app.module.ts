91d3b5ec2fe383b3e77e279b1ca4836b204da78b
Pavlo_Netrebskyi <Pavlo_Netrebskyi@epam.com>
2018-03-06 17:04:00 +0200

    Create layout, header, footer, menu components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavMenuComponent } from './layout/nav-menu/nav-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderMenuComponent]
+})
+export class LayoutModule { }
