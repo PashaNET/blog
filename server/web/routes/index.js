9358f539383c8747113ac7f1ec11a086340668b4
PavelNET <pavlo.netrebsky@gmail.com>
2018-03-10 11:38:11 +0200

    Added test connection to mongoAtlas

--- server/web/routes/index.js
+++ server/web/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
+function connectToDB() {
+  
+
+  var uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test";
+  MongoClient.connect(uri, function(err, client) {
+    const collection = client.db("test").collection("devices");
+    console.log(collection);
+    client.close();
+  });
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Resonse');
+
+  let data = connectToDB();
+
+  res.send('Response');
 });
+
 
 module.exports = router;
