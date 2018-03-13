3cbf72c2224d646744d54d916b74b7c1497dda9c
PavelNET <pavlo.netrebsky@gmail.com>
2018-03-13 21:13:16 +0200

    Mongo connection

--- server/web/routes/index.js
+++ server/web/routes/index.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient;
 function connectToDB() {
   
 
-  var uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test";
+  var uri = "mongodb://PashaNET:mongo1048!@devcluster-shard-00-00-3tzqq.mongodb.net:27017,devcluster-shard-00-01-3tzqq.mongodb.net:27017,devcluster-shard-00-02-3tzqq.mongodb.net:27017/test?ssl=true&replicaSet=DevCluster-shard-0&authSource=admin";
   MongoClient.connect(uri, function(err, client) {
     const collection = client.db("test").collection("devices");
     console.log(collection);
