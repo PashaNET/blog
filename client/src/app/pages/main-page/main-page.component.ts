89d8997d3ea090da70bd495a0f6de83004999be3
Pavel <pavlo.netrebsky@gmail.com>
2018-03-16 09:40:41 +0200

    Update main-page.component.ts

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MainPageComponent implements OnInit {
 
   constructor() { }
-
+  //push to browser history, post/:post_id
   ngOnInit() {
   }
 
