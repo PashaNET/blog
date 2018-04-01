e5e48390a95993eee79e598eb5876368bda316ce
PashaNET <pavlo.netrebsky@gmail.com>
2018-04-01 19:44:34 +0300

    PostInfo, binded with data

--- client/src/app/components/post-info/post-info.component.ts
+++ client/src/app/components/post-info/post-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-info',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-info.component.styl']
 })
 export class PostInfoComponent implements OnInit {
-
+  @Input() name: String;
+  @Input() date: String;
+  @Input() category: String;
+  @Input() commentsQuantity: Number;
+  
   constructor() { }
 
   ngOnInit() {
